d7dfa5f07faf079241512fb487ec55aee54c561c
André Monteiro de Oliveira <andremo@ad.finep.gov.br>
2019-04-12 16:50:08 -0300

    teste material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatSliderModule} from '@angular/material/slider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,6 +17,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     BrowserAnimationsModule,
     MatSliderModule,
+    MatButtonModule,
+    MatIconModule,
     AppRoutingModule
   ],
   providers: [],
