16faceee298e14d4d09888322c69bd8f2e56085b
André Monteiro de Oliveira <andremo@ad.finep.gov.br>
2019-04-16 16:09:31 -0300

    npm update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatListModule} from '@angular/material/list';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 import {MatSliderModule} from '@angular/material/slider';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
@@ -17,6 +21,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MatCardModule,
+    MatRadioModule,
+    MatSlideToggleModule,
+    MatListModule,
     MatSliderModule,
     MatButtonModule,
     MatIconModule,
