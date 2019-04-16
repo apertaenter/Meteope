3ac574d8186aaef5bb54ad4f25b912f4f1076854
André Monteiro de Oliveira <andremo@ad.finep.gov.br>
2019-04-16 07:23:55 -0300

    Mat-Grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatSliderModule} from '@angular/material/slider';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,6 +20,7 @@ import { AppComponent } from './app.component';
     MatSliderModule,
     MatButtonModule,
     MatIconModule,
+    MatGridListModule,
     AppRoutingModule
   ],
   providers: [],
